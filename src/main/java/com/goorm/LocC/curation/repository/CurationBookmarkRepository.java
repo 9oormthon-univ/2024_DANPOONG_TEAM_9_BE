87ce342e524c43072cf0c9eeaed4c4a10f0f1d27
chaeyounglim <limcy423@gmail.com>
2024-11-20 02:59:34 +0900

    Feat(#5): 저장한 큐레이션 조회 기능 추가

--- src/main/java/com/goorm/LocC/curation/repository/CurationBookmarkRepository.java
+++ src/main/java/com/goorm/LocC/curation/repository/CurationBookmarkRepository.java
@@ -3,6 +3,6 @@ package com.goorm.LocC.curation.repository;
 import com.goorm.LocC.curation.domain.CurationBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CurationBookmarkRepository extends JpaRepository<CurationBookmark, Long> {
+public interface CurationBookmarkRepository extends JpaRepository<CurationBookmark, Long>, CurationBookmarkRepositoryCustom {
 
 }
