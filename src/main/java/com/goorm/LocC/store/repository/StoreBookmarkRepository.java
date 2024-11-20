18341cf0e6760f567ffd43459a9a8f949f934cb8
chaeyounglim <limcy423@gmail.com>
2024-11-20 18:36:38 +0900

    Feat(#4): 가게 북마크 토글 기능 추가

--- src/main/java/com/goorm/LocC/store/repository/StoreBookmarkRepository.java
+++ src/main/java/com/goorm/LocC/store/repository/StoreBookmarkRepository.java
@@ -1,8 +1,13 @@
 package com.goorm.LocC.store.repository;
 
+import com.goorm.LocC.member.domain.Member;
+import com.goorm.LocC.store.domain.Store;
 import com.goorm.LocC.store.domain.StoreBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoreBookmarkRepository extends JpaRepository<StoreBookmark, Long>, StoreBookmarkRepositoryCustom {
 
+    Optional<StoreBookmark> findStoreBookmarkByMemberAndStore(Member member, Store stroe);
 }
