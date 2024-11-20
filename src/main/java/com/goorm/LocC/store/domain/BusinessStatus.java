a20283418177ecfec1decf4675883fc72ec109c6
chaeyounglim <limcy423@gmail.com>
2024-11-20 14:46:58 +0900

    Feat(#5): 영업상태 enum 생성

---
+++ src/main/java/com/goorm/LocC/store/domain/BusinessStatus.java
@@ -0,0 +1,52 @@
+package com.goorm.LocC.store.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.goorm.LocC.store.exception.StoreException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.goorm.LocC.store.exception.StoreErrorCode.INVALID_BUSINESS_STATUS_ENUM;
+
+@Getter
+@RequiredArgsConstructor
+public enum BusinessStatus {
+    OPEN("영업중"),
+    CLOSE("영업종료"),
+    HOLIDAY("휴무일"),
+    ;
+
+    private final String name;
+    private final static Map<String, BusinessStatus> NAME_TO_ENUM_MAP = new HashMap<>();
+
+    static {
+        for (BusinessStatus businessStatus : BusinessStatus.values()) {
+            NAME_TO_ENUM_MAP.put(businessStatus.name, businessStatus);
+        }
+    }
+
+    @JsonValue
+    public String getName() {
+        return name;
+    }
+
+    @JsonCreator
+    public static BusinessStatus fromName(String name) {
+        BusinessStatus businessStatus = NAME_TO_ENUM_MAP.get(name);
+
+        if (businessStatus == null) {
+            throw new StoreException(INVALID_BUSINESS_STATUS_ENUM);
+        }
+
+        return businessStatus;
+    }
+
+    public static BusinessStatus checkBusinessStatus(boolean isHoliday, LocalTime openTime, LocalTime closeTime) {
+        return isHoliday ? HOLIDAY :
+                LocalTime.now().isAfter(openTime) && LocalTime.now().isBefore(closeTime) ? OPEN : CLOSE;
+    }
+}
