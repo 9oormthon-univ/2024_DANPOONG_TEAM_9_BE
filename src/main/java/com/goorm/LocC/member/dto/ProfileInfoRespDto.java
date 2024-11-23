dad2229458cf4dc640dffafda7fba7479042cd02
chaeyounglim <limcy423@gmail.com>
2024-11-23 22:56:34 +0900

    Feat: 마이페이지 조회 시 선호 지역, 카테고리들 반환 추가

--- src/main/java/com/goorm/LocC/member/dto/ProfileInfoRespDto.java
+++ src/main/java/com/goorm/LocC/member/dto/ProfileInfoRespDto.java
@@ -2,6 +2,9 @@ package com.goorm.LocC.member.dto;
 
 import com.goorm.LocC.curation.dto.CurationInfoDto;
 import com.goorm.LocC.member.domain.Member;
+import com.goorm.LocC.store.domain.Category;
+import com.goorm.LocC.store.domain.City;
+import com.goorm.LocC.store.domain.Province;
 import com.goorm.LocC.store.dto.StoreInfoDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
@@ -25,14 +28,23 @@ public class ProfileInfoRespDto {
     private List<CurationInfoDto> savedCurations;
     @Schema(description = "저장한 가게 리스트")
     private List<StoreInfoDto> savedStores;
+    @Schema(description = "선호하는 지역", example = "강원")
+    private Province preferredProvince;
+    @Schema(description = "선호하는 도시", example = "강릉시")
+    private City preferredCity;
+    @Schema(description = "선호하는 카테고리 리스트", example = "[\"카페, 맛집\"]")
+    private List<Category> preferredCategories;
 
     @Builder
-    public ProfileInfoRespDto(String username, String handle, String profileImageUrl, List<CurationInfoDto> savedCurations, List<StoreInfoDto> savedStores) {
+    public ProfileInfoRespDto(String username, String handle, String profileImageUrl, List<CurationInfoDto> savedCurations, List<StoreInfoDto> savedStores, Province preferredProvince, City preferredCity, List<Category> preferredCategories) {
         this.username = username;
         this.handle = handle;
         this.profileImageUrl = profileImageUrl;
         this.savedCurations = savedCurations;
         this.savedStores = savedStores;
+        this.preferredProvince = preferredProvince;
+        this.preferredCity = preferredCity;
+        this.preferredCategories = preferredCategories;
     }
 
     public static ProfileInfoRespDto of(Member member, List<CurationInfoDto> curations, List<StoreInfoDto> stores) {
@@ -42,6 +54,10 @@ public class ProfileInfoRespDto {
                 .profileImageUrl(member.getProfileImageUrl())
                 .savedCurations(curations)
                 .savedStores(stores)
+                .preferredProvince(member.getPreferredProvince())
+                .preferredCity(member.getPreferredCity())
+                .preferredCategories(member.getPreferredCategories()
+                        .stream().map(pc -> pc.getCategory()).toList())
                 .build();
     }
 }
